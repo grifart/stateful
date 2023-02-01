3e1a5eeb0a8711a3240a665b296731fc441ff2b3
Daniel Kurowski <daniel.kurowski@grifart.cz>
2023-02-01 10:48:30 +0100

    introduce trait for enums

---
+++ src/StatefulBackedEnum.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types = 1);
+
+namespace Grifart\Stateful;
+
+
+trait StatefulBackedEnum
+{
+	public function _getState(): State
+	{
+		return StateBuilder::from($this)
+			->version(1)
+			->field('value', $this->value)
+			->ignore('name')
+			->build();
+	}
+
+	public static function _fromState(State $state): static
+	{
+		$state->ensureVersion(1);
+		$state->ignoreProperty(['name']);
+		/** @var array{value: int|string} $state */
+		return self::from($state['value']);
+	}
+}
